6d91d1bc0f75cf0d084dd47a14808b8d7c954a93
FUJI Goro (gfx) <gfuji@cpan.org>
2017-11-22 15:10:20 +0900

    fix indent

--- src/index.ts
+++ src/index.ts
@@ -18,44 +18,44 @@
 const z = require("./libzopfli");
 
 enum ZopfliFormat {
-    GZIP,
-    ZLIB,
-    DEFLATE,
+  GZIP,
+  ZLIB,
+  DEFLATE,
 }
 
 // https://github.com/google/zopfli/blob/master/src/zopfli/zopfli.h
 export interface ZopfliOptions {
-    /** Whether to print output */
-    verbose?: boolean;
+  /** Whether to print output */
+  verbose?: boolean;
 
-    /** Whether to print more detailed output  */
-    verbose_more?: boolean;
-    /**
-        Maximum amount of times to rerun forward and backward pass to optimize LZ77
-        compression cost. Good values: 10, 15 for small files, 5 for files over
-        several MB in size or it will be too slow.
-     */
-    numiterations?: number;
-    /**
-        If true, splits the data in multiple deflate blocks with optimal choice
-        for the block boundaries. Block splitting gives better compression. Default:
-        true (1).
-     */
-    blocksplitting?: boolean;
+  /** Whether to print more detailed output  */
+  verbose_more?: boolean;
+  /**
+      Maximum amount of times to rerun forward and backward pass to optimize LZ77
+      compression cost. Good values: 10, 15 for small files, 5 for files over
+      several MB in size or it will be too slow.
+   */
+  numiterations?: number;
+  /**
+      If true, splits the data in multiple deflate blocks with optimal choice
+      for the block boundaries. Block splitting gives better compression. Default:
+      true (1).
+   */
+  blocksplitting?: boolean;
 
-    /**
-        Maximum amount of blocks to split into (0 for unlimited, but this can give
-        extreme results that hurt compression on some files). Default value: 15.
-     */
-    blocksplittingmax?: number;
+  /**
+      Maximum amount of blocks to split into (0 for unlimited, but this can give
+      extreme results that hurt compression on some files). Default value: 15.
+   */
+  blocksplittingmax?: number;
 }
 
 const defaultOptions: ZopfliOptions = {
-    verbose: false,
-    verbose_more: false,
-    numiterations: 15,
-    blocksplitting: true,
-    blocksplittingmax: 15,
+  verbose: false,
+  verbose_more: false,
+  numiterations: 15,
+  blocksplitting: true,
+  blocksplittingmax: 15,
 };
 
 export type OnCompressComplete = (err: Error | null, buffer: Uint8Array) => void;
@@ -66,88 +66,88 @@ type Task = () => void;
 let queue: Array<Task> | null = new Array<Task>();
 
 z.onRuntimeInitialized = () => {
-    for (const task of queue!) {
-        task();
-    }
-    queue = null;
+  for (const task of queue!) {
+    task();
+  }
+  queue = null;
 };
 
 function ensureByteBuffer(input: InputType): Array<number> | Uint8Array {
-    if (typeof input === 'string') {
-        const a = z.intArrayFromString(input);
-        a.length--; // because emscripten's intArrayFromString() adds trailing nul
-        return a;
-    } else {
-        return input;
-    }
+  if (typeof input === 'string') {
+    const a = z.intArrayFromString(input);
+    a.length--; // because emscripten's intArrayFromString() adds trailing nul
+    return a;
+  } else {
+    return input;
+  }
 }
 
 function callCompress(input: InputType, format: ZopfliFormat, options: ZopfliOptions, cb: OnCompressComplete) {
-    console.assert(input != null, "buffer must not be null");
-    console.assert(options != null, "options must not be null");
-    console.assert(cb != null, "cb must not be null");
+  console.assert(input != null, "buffer must not be null");
+  console.assert(options != null, "options must not be null");
+  console.assert(cb != null, "cb must not be null");
 
-    const byteBuffer = ensureByteBuffer(input);
-    const bufferPtr = z.allocate(byteBuffer, 'i8', z.ALLOC_NORMAL);
+  const byteBuffer = ensureByteBuffer(input);
+  const bufferPtr = z.allocate(byteBuffer, 'i8', z.ALLOC_NORMAL);
 
-    const opts = { ...defaultOptions, ...options };
+  const opts = { ...defaultOptions, ...options };
 
-    const output = z._createZopfliJsOutput();
-    z._compress(bufferPtr, byteBuffer.length, output,
-        format,
-        opts.verbose,
-        opts.verbose_more,
-        opts.numiterations,
-        opts.blocksplitting,
-        opts.blocksplittingmax,
-    );
+  const output = z._createZopfliJsOutput();
+  z._compress(bufferPtr, byteBuffer.length, output,
+    format,
+    opts.verbose,
+    opts.verbose_more,
+    opts.numiterations,
+    opts.blocksplitting,
+    opts.blocksplittingmax,
+  );
 
-    const outputPtr = z._getBuffer(output);
-    const outputSize = z._getBufferSize(output);
+  const outputPtr = z._getBuffer(output);
+  const outputSize = z._getBufferSize(output);
 
-    const result = new Uint8Array(z.HEAP8.subarray(outputPtr, outputPtr + outputSize));
-    z._free(outputPtr);
-    z._free(output);
-    z._free(bufferPtr);
+  const result = new Uint8Array(z.HEAP8.subarray(outputPtr, outputPtr + outputSize));
+  z._free(outputPtr);
+  z._free(output);
+  z._free(bufferPtr);
 
-    // zopfli does not fail unless a violation of preconditions occurs.
-    cb(null, result);
+  // zopfli does not fail unless a violation of preconditions occurs.
+  cb(null, result);
 }
 
 function compress(buffer: InputType, format: ZopfliFormat, options: ZopfliOptions, cb: OnCompressComplete) {
-    if (queue) {
-        queue.push(() => {
-            callCompress(buffer, format, options, cb);
-        });
-    } else {
-        callCompress(buffer, format, options, cb);
-    }
+  if (queue) {
+    queue.push(() => {
+      callCompress(buffer, format, options, cb);
+    });
+  } else {
+    callCompress(buffer, format, options, cb);
+  }
 }
 
 export function gzip(buffer: InputType, options: ZopfliOptions, cb: OnCompressComplete) {
-    compress(buffer, ZopfliFormat.GZIP, options, cb);
+  compress(buffer, ZopfliFormat.GZIP, options, cb);
 }
 
 export function zlib(buffer: InputType, options: ZopfliOptions, cb: OnCompressComplete) {
-    compress(buffer, ZopfliFormat.ZLIB, options, cb);
+  compress(buffer, ZopfliFormat.ZLIB, options, cb);
 }
 
 export function deflate(buffer: InputType, options: ZopfliOptions, cb: OnCompressComplete) {
-    compress(buffer, ZopfliFormat.DEFLATE, options, cb);
+  compress(buffer, ZopfliFormat.DEFLATE, options, cb);
 }
 
 function promisify(f: (buffer: InputType, options: ZopfliOptions, cb: OnCompressComplete) => void) {
-    return (buffer: InputType, options: ZopfliOptions) => {
-        return new Promise<Uint8Array>((resolve, reject) => {
-            f(buffer, options, (err, result) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(result);
-                }
-            });
-        });
-    };
+  return (buffer: InputType, options: ZopfliOptions) => {
+    return new Promise<Uint8Array>((resolve, reject) => {
+      f(buffer, options, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  };
 }
 
 export const gzipAsync = promisify(gzip);
