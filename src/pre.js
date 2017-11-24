9aaad3c97b80698dcdb35ff78076d9d393752e2b
FUJI Goro (gfx) <gfuji@cpan.org>
2017-11-24 11:43:05 +0900

    fix names; wasm64 seems "64-bit wasm"

--- src/pre.js
+++ src/pre.js
@@ -15,11 +15,11 @@
 */
 
 const base64 = require('base64-js');
-const wasm64 = require('./libzopfli-wasm');
+const base64EncodedBinary = require('./libzopfli-wasm');
 
 // The emscripten's Module object.
 // See http://kripken.github.io/emscripten-site/docs/api_reference/module.html for details.
 var Module = {};
 
 Module.noInitialRun = true;
-Module.wasmBinary = base64.toByteArray(wasm64);
+Module.wasmBinary = base64.toByteArray(base64EncodedBinary);
