24601b27a5284a3668ffbce49fdfc34629fdc909
FUJI Goro (gfx) <gfuji@cpan.org>
2019-05-17 14:29:45 +0900

    add tests

--- test/gzip_test.ts
+++ test/gzip_test.ts
@@ -1,11 +1,37 @@
-import * as zlib from 'zlib';
-import * as util from 'util';
-import * as fs from 'fs';
+import zlib from 'zlib';
+import util from 'util';
+import fs from 'fs';
+import base64 from 'base64-js'
 
 import test from 'ava';
-import { gzipAsync as gzip } from '../dist';
+import { gzipAsync as gzip, deflateAsync, zlibAsync } from '../dist';
 
-const gunzip = util.promisify(zlib.gunzip) as any;
+const gunzip = util.promisify(zlib.gunzip);
+
+test('gzip, compared with `zopfli --gzip`', async (t) => {
+  const s = "foo";
+  const gz = await gzip(s, {});
+  t.deepEqual(gz, base64.toByteArray("H4sIAAAAAAACA0vLzwcAIWVzjAMAAAA="));
+});
+
+test('zlib, compared with `zopfli --zlib`', async (t) => {
+  const s = "foo";
+  const zlib = await zlibAsync(s, {});
+  t.deepEqual(zlib, base64.toByteArray("eNpLy88HAAKCAUU="));
+});
+
+test('deflte, compared wit `zopfli --deflate`', async (t) => {
+  const s = "foo";
+  const deflate = await deflateAsync(s, {});
+  t.deepEqual(deflate, base64.toByteArray("S8vPBwA="));
+});
+
+test('gzip, compared wit `zopfli --zlib`', async (t) => {
+  const s = "foo";
+  const gz = await gzip(s, {});
+  // the data is creted by `zopfli --gzip`
+  t.deepEqual(gz, base64.toByteArray("H4sIAAAAAAACA0vLzwcAIWVzjAMAAAA="));
+});
 
 test('short string', async (t) => {
   const s = "foo";
